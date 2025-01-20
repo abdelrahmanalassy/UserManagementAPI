a84fd2f541058ed82e1c5142e7108c6b563a4a7d
Abdelrahman Alassy <abdelrahmanalassy@gmail.com>
2025-01-20 13:55:17 +0200

    Initial commit

--- obj/Debug/net8.0/UserManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410ade7263c7b0106173bd2f3e684020611653d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
